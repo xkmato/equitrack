66a9d67a27feddeeb711c0d5185ec12cd244ae44
Kenneth Matovu <matovken@gmail.com>
2014-07-16 17:07:43 +0300

    adjust something small

--- equitrack/models.py
+++ equitrack/models.py
@@ -10,15 +10,21 @@ class IPartners(models.Model):
     ip_type = models.CharField(max_length=100, blank=True)
     PCA_number = models.CharField(max_length=100)
 
+    def __unicode__(self):
+        return self.ip_name
+
 
 class FACE(models.Model):
     ref = models.CharField(max_length=100)
     partner = models.ForeignKey(IPartners, related_name='face_refs')
     submited_on = models.DateTimeField(auto_now_add=True)
-    amount = models.CharField(max_length=100)
+    amount = models.CharField(max_length=100, default=0)
     paid = models.BooleanField(default=False)
     date_paid = models.DateTimeField(auto_now_add=True)
     acknowledged = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.partner.ip_name + "FACEs"
 
     def generate_number(self):
         m = hashlib.md5()
@@ -26,7 +32,8 @@ class FACE(models.Model):
         self.ref = m.digest()
 
     def save(self, force_insert=False, force_update=False, using=None):
-        self.generate_number()
+        if not  self.ref:
+            self.generate_number()
         if self.paid:
             pass
             #Tell API that this fellow has been paid
--- equitrack/views.py
+++ equitrack/views.py
@@ -1,9 +1,19 @@
+import json
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from equitrack.models import IPartners, FACE
 
 __author__ = 'kenneth'
 
 @csrf_exempt
 def home(request):
-    print request.POST
-    return HttpResponse(status=201)
+    text = request.POST.get('text', None)
+    response = json.dumps({'error':'One of the values is missing'})
+    if text and len(text) > 1:
+        ip = IPartners.objects.get(PCA=text[0])
+        face = FACE.objects.create(partner=ip)
+        face.amount = text[1]
+        face.partner = ip
+        face.save()
+        response = json.dumps({'extras': {'faceref':face.ref}})
+    return HttpResponse(response)
