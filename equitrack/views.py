3a5f278f5d6914d9f2e5caf15f6a3799d1eb0f4a
Kenneth Matovu <matovken@gmail.com>
2014-07-16 17:12:35 +0300

    adjust something small number

--- equitrack/views.py
+++ equitrack/views.py
@@ -8,11 +8,12 @@ __author__ = 'kenneth'
 @csrf_exempt
 def home(request):
     text = request.POST.get('text', None)
+    print text
     response = json.dumps({'error':'One of the values is missing'})
     if text and len(text) > 1:
-        ip = IPartners.objects.get(PCA_number=text[0])
+        ip = IPartners.objects.get(PCA_number=text[1])
         face = FACE.objects.create(partner=ip)
-        face.amount = text[1]
+        face.amount = text[0]
         face.partner = ip
         face.save()
         response = json.dumps({'extras': {'faceref':face.ref}})
