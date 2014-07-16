45b7c701db53b146fc03dfa5e4637f1a468739a8
Kenneth Matovu <matovken@gmail.com>
2014-07-16 16:33:07 +0300

    csrf ex

--- equitrack/views.py
+++ equitrack/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 __author__ = 'kenneth'
 
-
+@csrf_exempt
 def home(request):
     print request.body
     return HttpResponse(status=201)
