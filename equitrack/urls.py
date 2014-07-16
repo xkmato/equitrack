67be4939722ee4ee7b42b5a7a78a7eb9e83e8df0
Kenneth Matovu <matovken@gmail.com>
2014-07-16 16:29:26 +0300

    sample view

--- equitrack/urls.py
+++ equitrack/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'equitrack.views.home', name='home'),
+    url(r'^pc_number$', 'equitrack.views.home', name='home'),
     # url(r'^equitrack/', include('equitrack.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- equitrack/views.py
+++ equitrack/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
+
 __author__ = 'kenneth'
 
 
+def home(request):
+    print request.body
+    return HttpResponse(status=201)
